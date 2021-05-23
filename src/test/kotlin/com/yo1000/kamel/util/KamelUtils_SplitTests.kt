b94e91d505a13ee3adc561b739869c1aade8fd9e
yo1000 <i@yo1000.com>
2021-05-24 02:53:34 +0900

    Add split test case for README example

--- src/test/kotlin/com/yo1000/kamel/util/KamelUtils_SplitTests.kt
+++ src/test/kotlin/com/yo1000/kamel/util/KamelUtils_SplitTests.kt
@@ -55,6 +55,55 @@ class KamelUtils_SplitTests {
     }
 
     @Test
+    fun testSplitAndAggregateSum() {
+        val context = DefaultCamelContext().also {
+            it.addRoutes(SplitAndAggregateSumTestRouteBuilder())
+            it.start()
+        }
+
+        val producerTemplate = context.createProducerTemplate()
+
+        producerTemplate.send(
+            "direct://${SplitAndAggregateSumTestRouteBuilder::class.simpleName}",
+            DefaultExchange(context).also {
+                it.message.body = (1..3).toList()
+            }
+        ).let {
+            Assertions.assertThat(it.message.body).isEqualTo(90)
+        }
+    }
+
+    class SplitAndAggregateSumTestRouteBuilder : RouteBuilder() {
+        override fun configure() {
+            from("direct://${SplitAndAggregateSumTestRouteBuilder::class.simpleName}")
+                .process {
+                    it.message.body = (1..3)
+                }
+                .split(body(), {
+                    it.process {
+                        it.message.body = it.message.getBody(Int::class)?.let { it + 1 }
+                    }
+
+                    it.process {
+                        it.message.body = it.message.getBody(Int::class)?.let { it * 10 }
+                    }
+                }, { prevExchange, currentExchange ->
+                    if (prevExchange == null) {
+                        currentExchange
+                    } else {
+                        val prev = prevExchange.message.getBody(Int::class)!!
+                        val curr = currentExchange.message.getBody(Int::class)!!
+                        prevExchange.message.body = prev + curr
+                        prevExchange
+                    }
+                })
+                .process {
+                    println(it.message.body)
+                }
+        }
+    }
+
+    @Test
     fun testSplit() {
         val syncList: MutableList<String> = Collections.synchronizedList(mutableListOf())
 
